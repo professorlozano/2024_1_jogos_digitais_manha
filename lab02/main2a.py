960d1944fe4fdfcd746720fa864fc2b9c9191d93
Professor Lozano <prof-luiz@outlook.com>
2024-02-16 08:36:16 -0300

    lab2_final

--- lab02/main2a.py
+++ lab02/main2a.py
@@ -8,8 +8,13 @@ screen = pygame.display.set_mode((600,400))
 loop = True
 while loop:
     for event in pygame.event.get():
-        screen.fill((255,255,255)) #preenche a tela com uma determinada cor
-        pygame.display.flip() #atualiza o display
+        if event.type == pygame.QUIT:
+            loop = False
+            pygame.quit()
+            exit()
+
+    screen.fill((255,255,255)) #preenche a tela com uma determinada cor
+    pygame.display.flip() #atualiza o display
 
 
     
